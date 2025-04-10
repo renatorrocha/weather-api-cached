d2f597d5be54e921e909e9376381ad671e4baf77
renatorrocha <renatorrodrigues2002@gmail.com>
2025-04-10 00:06:11 -0300

    feat: implement caching for weather data in the weather service

--- src/modules/weather/service.go
+++ src/modules/weather/service.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
+	"time"
 )
 
 type WeatherService interface {
@@ -14,18 +16,31 @@ type WeatherService interface {
 
 type weatherServiceImpl struct {
 	apiKey string
+	cache  map[string]cachedData
+}
+
+type cachedData struct {
+	Data      string
+	ExpiresAt time.Time
 }
 
 func NewWeatherService(apiKey string) WeatherService {
 	return &weatherServiceImpl{
 		apiKey: apiKey,
+		cache:  make(map[string]cachedData),
 	}
 }
 
 func (s *weatherServiceImpl) Get(city string) (string, error) {
+
 	cityParam := url.QueryEscape(city)
+	if val, ok := s.cache[cityParam]; ok && time.Now().Before(val.ExpiresAt) {
+		return val.Data, nil
+	}
 
 	fullUrl := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", cityParam, s.apiKey)
+
+	log.Println("Fetching weather data for", city)
 
 	response, err := http.Get(fullUrl)
 	if err != nil {
@@ -42,6 +57,11 @@ func (s *weatherServiceImpl) Get(city string) (string, error) {
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
 		return "", err
+	}
+
+	s.cache[cityParam] = cachedData{
+		Data:      string(body),
+		ExpiresAt: time.Now().Add(time.Minute * 10),
 	}
 
 	return string(body), nil
